fd0aa3a20670cebf27a9b47fcc831685dc937a63
FloiceNyota98 <nyotafloice@gmail.com>
2021-10-31 00:24:51 -0400

    index template and view logic add

--- instaClone/views.py
+++ instaClone/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post, Profile, Location, Comments
 
-# Create your views here.
+def index(request):
+  allpictures = Post.all_pictures()
+  content = {
+    'allpictures':allpictures,
+  }
+  return render(request, 'index.html', content)
