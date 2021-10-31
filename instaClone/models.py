d0522b7b1a829e64566b9370be259ff42da3b6a4
FloiceNyota98 <nyotafloice@gmail.com>
2021-10-30 21:56:54 -0400

    location model add

--- instaClone/models.py
+++ instaClone/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+class Location(models.Model):
+  location = models.CharField(max_length=100)
+
+  def saveLocation(self):
+    self.save()
+
+  @classmethod
+  def deleteLocation(cls, id):
+    cls.objects.filter(id=id).delete()
+
+  @classmethod
+  def updateLocation(cls, id, locaUpdate):
+    cls.objects.filter(id=id).update(location=locaUpdate)
+
+  def __str__(self):
+    return self.location
 
 # Create your models here.
