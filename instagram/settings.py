4f9d5a46cb81ff8b2c7be1ac561d7b2d9478cf5a
FloiceNyota98 <nyotafloice@gmail.com>
2021-10-30 21:50:40 -0400

    urls added both project and app

--- instagram/settings.py
+++ instagram/settings.py
@@ -18,8 +18,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap5',
-    'instaclone',
+    "bootstrap5",
+    'instaClone',
 ]
 
 MIDDLEWARE = [
