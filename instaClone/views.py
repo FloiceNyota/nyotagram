5041e8a23cd9455fe8f0f38659aecd5555a49a2b
FloiceNyota98 <nyotafloice@gmail.com>
2021-10-31 01:27:36 -0400

    index data add pot and users

--- instaClone/views.py
+++ instaClone/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from .models import Post, Profile, Location, Comments
 
 def index(request):
+  allusers = Profile.objects.all()
   allpictures = Post.all_pictures()
   content = {
     'allpictures':allpictures,
+    'allusers': allusers,
   }
   return render(request, 'index.html', content)
